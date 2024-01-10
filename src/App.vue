d7c5458ceb69eb6e7be8c810315a370639bfa801
ruhulrahman2233 <ruhulrahman2233@gmail.com>
2024-01-10 23:33:24 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="flex flex-row">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+@import './style.css';
+</style>
